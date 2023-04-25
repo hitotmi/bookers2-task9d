fadba17a260dba4995b908145049e109ba9e1dd7
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-25 23:59:31 +0000

    [Start] 応用課題9d

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @booknew = Book.new
+    @book_comment = BookComment.new
+  end
+
+  def index
+    if params[:latest]
+      @books = Book.latest
+    elsif
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :star)
+  end
+
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+ before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+    # @following_users = @user.following_user
+    # @follower_users = @user.follower_user
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+       redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+       render :edit
+    end
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  # フォローされた一覧
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
+end

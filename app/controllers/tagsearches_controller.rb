d751e0d42b9e60a328d25ff549f8f5db30086b91
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-26 01:11:18 +0000

    [Finish] 応用課題9d

--- app/controllers/tagsearches_controller.rb
+++ app/controllers/tagsearches_controller.rb
@@ -2,6 +2,5 @@ class TagsearchesController < ApplicationController
   def search
     @word = params[:word]
     @books = Book.where("tag LIKE?","%#{@word}%")
-    redirect_to tagsearches_search_path
   end
 end
